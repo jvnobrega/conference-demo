bef42a8f3bdcc6ed0dbfb8972976b7bd0be168db
Victor Nobrega <j.victorng@gmail.com>
2022-09-08 11:43:43 +0100

    CrossOrigin

--- src/main/java/com/pluralsight/conference/demo/controller/HomeController.java
+++ src/main/java/com/pluralsight/conference/demo/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.pluralsight.conference.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HomeController {
 
